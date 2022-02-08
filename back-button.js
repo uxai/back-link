6b576e842c12397b51ab56f5b20d283b627d810d
Matthew <matt.talebi@live.com>
2022-02-08 12:15:21 +0800

    Added script files

---
+++ back-button.js
@@ -0,0 +1,17 @@
+URL = 'https://uxai.net/'
+
+function takemeback(path) {
+    // Checks tab history exists and referrer value
+    if(history.legth > 1 && document.referrer != "") {
+        history.back()
+    } else {
+        if(path == 1) {
+            // Grabs the pathname and takes only the top level path
+            let top = window.location.pathname.split('/')[1]
+            goto = URL + top
+            window.location.href = goto;
+        } else {
+            window.location.href = URL;
+        }
+    }
+}
